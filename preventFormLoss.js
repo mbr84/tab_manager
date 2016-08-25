88cb4c5bbee7ed023db6396677ddfc70c4e1f2aa
mbr84 <matthew.bramfeld@gmail.com>
2016-08-25 12:12:08 -0700

    manifest

---
+++ preventFormLoss.js
@@ -0,0 +1,16 @@
+// // Feature to be implemented
+// let inputs = document.getElementsByTagName('input')
+// let texts = document.getElementsByTagName('textarea')
+//
+// const formFields = Array.from(inputs).concat(Array.from(texts))
+// formFields.forEach((field) => {
+//   field.addEventListener('onfocus', (e) => {
+//
+//   })
+// })
+// chrome.storage.local.get("protectedPages", function(result) {
+//   const a = document.createElement('a')
+//   a.href = tab.url
+//   result.protectedPages[a.hostname] = true
+//   chrome.storage.local.set({ protectedPages: result.protectedPages })
+// });
